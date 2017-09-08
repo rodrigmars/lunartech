4910b2aa55d331450e4e45d6d120fb4fc670842f
Rodrigo Silva <rodrigmars@gmail.com>
2017-09-08 15:33:54 -0300

    Ajustes para mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express'),
   app = express(),
-  calcs = require("./models/calcs");
+  calcs = require("./models/calcs"),
+  mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/lunartech');
+
+mongoose.connection.once('open', function() {
+  console.log('Connection open');
+}).on('error', function(error) {
+  console.log('Connection error', error);
+});
 
 // const port = process.env.PORT || 3000;
 
